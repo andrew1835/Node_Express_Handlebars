9565faef6f7db2eae471da257a69d901d0091781
Andrew Anthony <a.anthony1835@gmail.com>
2021-01-27 06:01:01 -0800

    trying to get heroku to work still

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "vqisc8nenqycbss0",
-  password: "kzplhavwwv2djieo",
-  database: "u9f8t0taa1fre6zi"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
+
 
 // Make connection.
 connection.connect(function (err) {
